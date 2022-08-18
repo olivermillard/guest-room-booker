8f189df35d6e6a912be74a69a36a604d08cebaf6
Oliver Millard <olivermillard@gmail.com>
2022-08-18 21:06:08 +0100

    client: added contact modal functionality

--- client/src/components/ContactFab.tsx
+++ client/src/components/ContactFab.tsx
@@ -7,13 +7,14 @@ import {
     Button,
     VStack
 } from 'native-base';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { ContactModal } from './ContactModal';
 
 type ContactName = 'Oliver' | 'Chris' | 'Both';
 
 const ContactFab = () => {
-    const navigate = useNavigate();
+    const [ showModal, setShowModal ] = useState(false);
+    const [ recipient, setRecipient ] = useState<ContactName>('Oliver');
 
     const {
         isOpen,
@@ -31,7 +32,10 @@ const ContactFab = () => {
                 colorScheme="blue"
                 borderRadius="full"
                 w='100px'
-                onPress={()=> navigate('/contact', { state: {recipient: name }})}
+                onPress={()=> {
+                    setRecipient(name);
+                    setShowModal(true);
+                }}
             >
                 {name}
             </Button>
@@ -39,77 +43,84 @@ const ContactFab = () => {
     };
 
     return(
-        <Box
-            position='fixed'
-            right='10px'
-            bottom='10px'
-            zIndex={'10000'}
-        >
-            <VStack
-                alignItems={'flex-end'}
+        <>
+            <ContactModal
+                recipient={recipient}
+                showModal={showModal}
+                setShowModal={setShowModal}
+            />
+            <Box
+                position='fixed'
+                right='10px'
+                bottom='10px'
+                zIndex={'10000'}
             >
-                <Box
-                    alignItems="center"
-                    w='100%'
+                <VStack
+                    alignItems={'flex-end'}
                 >
-                    <Stagger
-                        visible={isOpen}
-                        initial={{
-                            opacity: 0,
-                            scale: 0,
-                            translateY: 34
-                        }}
-                        animate={{
-                            translateY: 0,
-                            scale: 1,
-                            opacity: 1,
-                            transition: {
-                                type: 'spring',
-                                mass: 0.8,
-                                stagger: {
-                                    offset: 30,
-                                    reverse: true
-                                }
-                            }
-                        }}
-                        exit={{
-                            translateY: 34,
-                            scale: 0.5,
-                            opacity: 0,
-                            transition: {
-                                duration: 100,
-                                stagger: {
-                                    offset: 30,
-                                    reverse: true
-                                }
-                            }
-                        }}>
-                        {fabButton('Oliver')}
-                        {fabButton('Chris')}
-                        {fabButton('Both')}
-                    </Stagger>
-                </Box>
-                <HStack
-                    alignItems="center"
-                >
-                    <Button
-                        variant="solid"
-                        borderRadius="full"
-                        size="md"
-                        onPress={onToggle}
-                        _light={{
-                            bg: 'calendarBlue',
-                            shadow: 1,
-                            _pressed: { bg: 'calendarBlue' },
-                        }}
-                        w='100px'
-                        colorScheme="blue"
+                    <Box
+                        alignItems="center"
+                        w='100%'
                     >
-                        {baseButtonText}
-                    </Button>
-                </HStack>
-            </VStack>
-        </Box>
+                        <Stagger
+                            visible={isOpen}
+                            initial={{
+                                opacity: 0,
+                                scale: 0,
+                                translateY: 34
+                            }}
+                            animate={{
+                                translateY: 0,
+                                scale: 1,
+                                opacity: 1,
+                                transition: {
+                                    type: 'spring',
+                                    mass: 0.8,
+                                    stagger: {
+                                        offset: 30,
+                                        reverse: true
+                                    }
+                                }
+                            }}
+                            exit={{
+                                translateY: 34,
+                                scale: 0.5,
+                                opacity: 0,
+                                transition: {
+                                    duration: 100,
+                                    stagger: {
+                                        offset: 30,
+                                        reverse: true
+                                    }
+                                }
+                            }}>
+                            {fabButton('Oliver')}
+                            {fabButton('Chris')}
+                            {fabButton('Both')}
+                        </Stagger>
+                    </Box>
+                    <HStack
+                        alignItems="center"
+                    >
+                        <Button
+                            variant="solid"
+                            borderRadius="full"
+                            size="md"
+                            onPress={onToggle}
+                            _light={{
+                                bg: 'calendarBlue',
+                                shadow: 1,
+                                _pressed: { bg: 'calendarBlue' },
+                            }}
+                            w='100px'
+                            colorScheme="blue"
+                        >
+                            {baseButtonText}
+                        </Button>
+                    </HStack>
+                </VStack>
+            </Box>
+        </>
     );
 };
 
---
+++ client/src/components/ContactModal.tsx
@@ -0,0 +1,196 @@
+import React from 'react';
+import { Button, Modal, FormControl, Input, Center, TextArea, WarningOutlineIcon } from 'native-base';
+import { useState } from 'react';
+import { ContactName } from './ContactFab';
+import { handleTextChange, emailIsValid } from '../utils/text';
+import { BACKEND_URL } from '../config';
+
+interface ContactModalProps {
+    recipient: ContactName;
+    showModal: boolean;
+    setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+
+}
+
+const ContactModal = (props: ContactModalProps) => {
+    const [ name, setName ] = useState('');
+    const [ showNameError, setShowNameError ] = useState(false);
+
+    const [ email, setEmail ] = useState('');
+    const [ showEmailError, setShowEmailError ] = useState(false);
+
+    const [ message, setMessage ] = useState('');
+    const [ showMessageError, setShowMessageError ] = useState(false);
+
+    let recipientName: string = props.recipient; // casting to string to change the value
+
+    if(recipientName === 'Both') {
+        recipientName = 'Oliver & Chris';
+    }
+
+    const handleSubmit = () => {
+        // check if there is an invalid entry
+        const emailCheck = emailIsValid(email);
+        if(!name || !emailCheck || !message) {
+            // check if name is invalid
+            if(!name) {
+                setShowNameError(true);
+            }
+            else {
+                setShowNameError(false);
+            }
+            // check if email is invalid
+            if(!email) {
+                setShowEmailError(true);
+            }
+            // check if it is a valid email or not, not just if something is entered
+            else{
+                setShowEmailError(emailCheck);
+            }
+            // check if message is invalid
+            if(!message) {
+                setShowMessageError(true);
+            }
+            else {
+                setShowMessageError(false);
+            }
+
+            const oliverEmail = 'olivermillard@gmail.com';
+            const chrisEmail = 'chris961125@gmail.com';
+
+            let recipientEmails = [];
+            if (props.recipient === 'Oliver') {
+                recipientEmails = [ oliverEmail ];
+            }
+            else if (props.recipient === 'Chris') {
+                recipientEmails = [ chrisEmail ];
+            }
+            else {
+                recipientEmails = [ oliverEmail, chrisEmail ];
+            }
+
+            const postData = {
+                name: name,
+                email: email,
+                message: message,
+                recipientName: recipientName,
+                recipientEmails: recipientEmails,
+            };
+            console.log('INFO', postData);
+            fetch(`${BACKEND_URL}/contact-us`, { // ./bookings
+                method: 'POST',
+                body: JSON.stringify(postData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                console.log('(OBM) response:', response.json());
+            }).catch(error => {
+                console.error(error);
+            });
+        }
+    };
+
+    return (
+        <Center>
+            <Modal isOpen={props.showModal} onClose={() => props.setShowModal(false)}>
+                <Modal.Content
+                    maxWidth="390px"
+                >
+                    <Modal.CloseButton />
+                    <Modal.Header>
+                        {`Contact ${recipientName}`}
+                    </Modal.Header>
+                    <Modal.Body>
+                        {/* NAME */}
+                        <FormControl isRequired isInvalid={showNameError}>
+                            <FormControl.Label>
+                                {'Name'}
+                            </FormControl.Label>
+                            <Input
+                                onChangeText={text => {
+                                    handleTextChange(
+                                        text,
+                                        setName,
+                                        showNameError,
+                                        setShowNameError,
+                                    );
+                                }}
+                            />
+                            <FormControl.ErrorMessage
+                                leftIcon={<WarningOutlineIcon size="xs" />}
+                            >
+                                {'Please enter your name'}
+                            </FormControl.ErrorMessage>
+                        </FormControl>
+                        {/* EMAIL */}
+                        <FormControl mt="3" isRequired isInvalid={showEmailError}>
+                            <FormControl.Label>
+                                {'Email'}
+                            </FormControl.Label>
+                            <Input
+                                onChangeText={text => {
+                                    handleTextChange(
+                                        text,
+                                        setEmail,
+                                        showEmailError,
+                                        setShowEmailError,
+                                    );
+                                }}
+                            />
+                            <FormControl.ErrorMessage
+                                leftIcon={<WarningOutlineIcon size="xs" />}
+                            >
+                                {'Please enter a valid email address'}
+                            </FormControl.ErrorMessage>
+                        </FormControl>
+                        {/* MESSAGE */}
+                        <FormControl mt="3" isRequired isInvalid={showMessageError}>
+                            <FormControl.Label>
+                                {'Message'}
+                            </FormControl.Label>
+                            <TextArea
+                                h={20}
+                                placeholder="Write your message here"
+                                w="100%"
+                                onChangeText={text => {
+                                    handleTextChange(
+                                        text,
+                                        setMessage,
+                                        showMessageError,
+                                        setShowMessageError,
+                                    );
+                                }}
+                            />
+                            <FormControl.ErrorMessage
+                                leftIcon={<WarningOutlineIcon size="xs" />}
+                            >
+                                {'Please enter a message to send to us'}
+                            </FormControl.ErrorMessage>
+                        </FormControl>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button.Group space={2}>
+                            <Button variant="ghost" colorScheme="blueGray" onPress={() => {
+                                props.setShowModal(false);
+                            }}>
+                                {'Cancel'}
+                            </Button>
+                            <Button onPress={() => {
+                                handleSubmit();
+                            }}>
+                                {'Send'}
+                            </Button>
+                        </Button.Group>
+                    </Modal.Footer>
+                </Modal.Content>
+            </Modal>
+        </Center>
+    );
+};
+
+
+
+export {
+    ContactModal,
+};
